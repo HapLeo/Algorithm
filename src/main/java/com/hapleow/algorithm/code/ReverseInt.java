b94446531c1f9e690d9b8cfd05058773acde1a4b
e <hapleow@gmail.com>
2019-10-15 00:35:15 +0800

    '整数反转算法'

---
+++ src/main/java/com/hapleow/algorithm/code/ReverseInt.java
@@ -0,0 +1,26 @@
+package com.hapleow.algorithm.code;
+
+/**
+ * 反转一个整数，如果超出范围则返回0
+ */
+public class ReverseInt {
+
+    public int reverse(int x) {
+
+        if (x == 0) {
+            return 0;
+        }
+        int rev = 0;
+        while (x != 0) {
+            int pop = x % 10;
+            x /= 10;
+            if (rev > Integer.MAX_VALUE / 10 || rev == Integer.MAX_VALUE && pop > Integer.MAX_VALUE % 10)
+                return 0;
+            if (rev < Integer.MIN_VALUE / 10 || rev == Integer.MIN_VALUE && pop < Integer.MIN_VALUE % 10)
+                return 0;
+            rev = rev * 10 + pop;
+        }
+        return rev;
+    }
+
+}
