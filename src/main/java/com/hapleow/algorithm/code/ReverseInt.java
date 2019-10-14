fcdce73d4fd732b8b89de5f04a40744f3ad92bb9
HapLeo <hapleow@gmail.com>
2019-10-15 00:51:55 +0800

    'test2'

--- src/main/java/com/hapleow/algorithm/code/ReverseInt.java
+++ src/main/java/com/hapleow/algorithm/code/ReverseInt.java
@@ -19,7 +19,6 @@ public class ReverseInt {
             if (rev < Integer.MIN_VALUE / 10 || rev == Integer.MIN_VALUE && pop < Integer.MIN_VALUE % 10)
                 return 0;
             rev = rev * 10 + pop;
-
         }
         return rev;
     }
